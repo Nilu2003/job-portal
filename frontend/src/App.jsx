766b220d255c62ef240fc96e13b3cb0d3cac9d5f
Nilu Prusty <niluprusty98@gmail.com>
2026-03-31 18:46:26 +0530

    change app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,30 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "./component/Navbar"
-import Footer from "./component/Footer"
-import { useDispatch } from "react-redux"
-import { useEffect } from "react"
-import API from "./api/api.js"
-import { loginSuccess, logout } from "./features/auth/authSlice.js"
-
-
+import { useState, useEffect } from "react"
 
 function App() {
-   
-const dispatch=useDispatch() 
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-  const fetchUser= async () => {
-    try {
-      const res= await API.get("/users/getprofile")
-      // console.log(res.data.data);
-      
-      dispatch(loginSuccess(res.data.data))
-    } catch (error) {
-      dispatch(logout())  
-      }}
-      
-  fetchUser()
+    const fetchUser = async () => {
+      try {
+        const res = await API.get("/users/getprofile")
+        dispatch(loginSuccess(res.data.data))
+      } catch (error) {
+        dispatch(logout())
+      } finally {
+        setLoading(false)
+      }
     }
-,[dispatch])
 
+    fetchUser()
+  }, [dispatch])
 
+  if (loading) return <div>Loading...</div>
 
   return (
     <>
-    <Navbar/>
-    <Outlet/>
-    {/* <Footer/> */}
+      <Navbar />
+      <Outlet />
     </>
   )
 }
-
-export default App
